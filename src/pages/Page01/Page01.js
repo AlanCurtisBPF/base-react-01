edc39a2f25d75dc5eeaff66b952f7c5eef2a090f
Alan Curtis <alan.curtis@bcfranchise.com>
2020-08-03 19:02:27 -0600

    removed clutter from page01

--- src/pages/Page01/Page01.js
+++ src/pages/Page01/Page01.js
@@ -1,26 +1,13 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import AUser from "../../components/AUser/AUser";
 import AUserEdit from "../../components/AUserEdit/AUserEdit";
 import AUsers from "../../components/AUsers/AUsers";
 
 const Page01 = () => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(`http://localhost:4000/graphql?query={customers{name,age}}`)
-      .then((res) => {
-        console.log("back", res.data.data.customers);
-        setUsers(res.data.data.customers);
-      });
-  }, []);
-
   return (
     <div className="Page01">
       Page01
       <div>
-        {users ? users.map((user, i) => <div key={i}>{user.name}</div>) : null}
         <AUser />
         <AUserEdit />
         <AUsers />
